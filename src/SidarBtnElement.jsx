d1294827ec353c1b89bd32d6a91c7384c5742d20
olawanlejoel <rahulrajesh2101@gmail.com>
2025-02-16 00:10:15 +0530

    Improve drag overlay rendering.

--- src/SidarBtnElement.jsx
+++ src/SidarBtnElement.jsx
@@ -32,9 +32,11 @@ export const SidarBtnElementDragOverlay = ({webElement}) => {
     const {label,icon:Icon} = webElement.designerBtnElement
     
   return (
-    <Button variant = {"outline"} className="flex flex-col gap-2 h-[120px] w-[120px ] cursor-grab ">
-        <Icon className ="h-8 w-8 text-slate-700 cursor-grab"/>
-        <p className='text-xs'>{label}</p>
-    </Button>
+    <button
+    className={`flex flex-col max-w-20 gap-2`}
+  >
+    <Icon className =" text-slate-700 cursor-grab bg-accent rounded-md w-10 h-10 "/>
+    <p className="text-xs text-white text-center">{label}</p>
+  </button>
   )
 }
